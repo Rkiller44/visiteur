772e7c610c7db88220e2bd4c73762c60583aa79c
Maer32 <tianamah32@gmail.com>
2025-05-29 12:52:46 +0300

    Quelques mofif

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -6,8 +6,3 @@ CREATE TABLE IF NOT EXISTS Visiteur (
     nombre_jours INT NOT NULL,
     tarif_journalier DECIMAL(10,2) NOT NULL
 );
-
-INSERT INTO Visiteur (nom, nombre_jours, tarif_journalier) VALUES
-('Jean Dupont', 5, 50.00),
-('Marie Martin', 3, 60.50),
-('Pierre Durand', 7, 45.75);
